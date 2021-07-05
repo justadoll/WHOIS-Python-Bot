0fe9a43413b4c1490ffc05608ff83b5ace31703a
bot <stelzybol@protonmail.com>
2021-07-05 22:57:16 +0300

    request limits for get_provider via memcached

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 1) Python3 and pip must be installed on your PC
 2) sudo apt install python3-dev && sudo apt install gcc
-3) pip3 install -r requirements.txt OR if it bugged - run:
-pip3 install aiogram loguru ipinfo pyexcel pyexcel-xls PyYAML
+3) pip3 install -r requirements.txt OR if it bugged:
+```pip3 install aiogram loguru ipinfo pyexcel pyexcel-xls PyYAML```
+4) Install memcached ```sudo apt install memcached```
 4) Insert your tokens in config.yaml (you can get 'config_api' from https://ipinfo.io/account/token and 'token' from @botFather)
 5) Create dir 'files' in project directory
 6) run by 'python3 main.py'
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from aiogram.dispatcher.filters import Text
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from loguru import logger
 from random import randint
+from pymemcache.client import base
 import ipinfo
 import pprint
 import pyexcel
@@ -15,6 +16,10 @@ import re
 import yaml
 import requests
 import time
+import asyncio
+
+client = base.Client(('localhost', 11211))
+
 
 logger.add("info.json", format="{time} {level} {message}", level="INFO", rotation="5 MB", compression="zip", serialize=True)
 
@@ -34,13 +39,34 @@ def get_host(ipfo):
     except AttributeError:
         return "Not found"
 
+def appender(inp,out):
+    out = []
+    if(inp != []):
+        for a in inp:
+            out.append(a)
+        return out
+    else:
+        return out
+
+
+def check_host(userinpt):
+    result = []
+    ips = re.findall(r"\d+\.\d+\.\d+\.\d+",userinpt)
+    host = re.findall(r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", userinpt)
+    result = appender(ips, result)
+    logger.debug(ips)
+    #result = appender(host, result)
+    if result != []:
+        return result
+    else:
+        return None
 
 class Curwhois(StatesGroup):
     addr = State()
 
-
 class Single_whois(StatesGroup):
     addr = State()
+
 bot = Bot(token=yamldata['token'])
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
@@ -78,9 +104,12 @@ async def process_set_ip(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['addr'] = message.text
         ip_list = data['addr'].split('\n')
-        uid = message['from']['username']
         ri = str(randint(0,1000))
-        uname = uid+"_"+time.strftime('%Y-%m-%d', time.localtime(int(time.time())))+f'({ri})'+".xls"
+        if message.from_user.username == None:
+            uname = str(message.chat.id)+"_"+time.strftime('%Y-%m-%d', time.localtime(int(time.time())))+f'({ri})'+".xls"
+        else:
+            uid = message['from']['username']
+            uname = uid+"_"+time.strftime('%Y-%m-%d', time.localtime(int(time.time())))+f'({ri})'+".xls"
         arr = []
         logger.info(uname+'\n'+str(message.chat.id))
         for ip in ip_list:
@@ -104,25 +133,64 @@ async def proccess_whois(message: types.Message):
 @dp.message_handler(state=Single_whois.addr)
 async def process_set_ip(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['addr'] = message.text
-        msg_res = ""
-        ip_list = data['addr'].split('\n')
-        for ip in ip_list:
-            logger.info(str(message.chat.id)+":"+str(ip))
+        data['addr'] = check_host(message.text)
+        logger.debug(len(data['addr']))
+
+        userinfo = client.get(str(message.chat.id))
+        logger.debug("USERINFO:"+str(userinfo))
+        if data['addr'] == None:
+            #[-] просто нету айпих в сообщении
+            await bot.send_message(message.chat.id, "Host/IP not found!\nRead /help and try again")
+            await state.finish()
+        elif (len(data['addr'])>100):
+            #[-] дохуя айпих в месаге
+            await bot.send_message(message.chat.id,'Слишком много айпи в одном сообщении!')
+            await state.finish()
+        elif (userinfo != None and int(userinfo)>100):
+            #[-] если пользователь есть в мемкахе и у него статус больше 100
+            await bot.send_message(message.chat.id, "Упс!\nВы просканили больше 100 айпи\nПриходите через час :3")
+        else:
+            #[+] если юзера нету в мемкахе
+            msg_res = ""
             try:
-                tmp = handler.getDetails(ip)
-                msg_res += "Hostname: "+get_host(tmp)+"\n"
-                msg_res += "Organization: "+get_org(tmp)+"\n"
-                msg_res += "IP:"+tmp.ip+"\nCity: "+ tmp.city+"\nCountry:"+tmp.country+"\nRegion: "+tmp.region+"\n\n"
-            except requests.exceptions.HTTPError:
-                logger.error(str(message.chat.id)+":"+str(ip))
-                await message.reply("Invalid ip: "+ ip)
+                if userinfo == None:
+                    count = len(data['addr'])
+                else:
+                    count = int(userinfo) + len(data['addr'])
 
-        await message.reply(msg_res)
-        await state.finish()
+                client.set(str(message.chat.id), str(count), 3600)
+                await asyncio.sleep(1)
+                new_user_count = client.get(str(message.chat.id))
+                logger.debug("NEW USER COUNT:"+str(new_user_count))
+            except Exception as e:
+                logger.error(e)
 
+            for host in data['addr']:
+                logger.info(str(message.chat.id)+":"+str(host))
+                try:
+                    tmp = handler.getDetails(host)
+                    msg_res += \
+                        f"""Hostname: {get_host(tmp)}\nOrganization: {get_org(tmp)}\nIP:{tmp.ip}\nCity: {tmp.city}\nCountry:{tmp.country}\nRegion: {tmp.region}\n\n"""
+                except requests.exceptions.HTTPError:
+                    logger.error(str(message.chat.id)+":"+str(host))
+                    await message.reply("Invalid host/ip: "+ host)
+                except Exception as e:
+                    whoid = message.chat.id
+                    logger.error(f"{whoid}:{e}")
 
+            await message.reply(msg_res)
+            await state.finish()
 
+@dp.message_handler(commands=['test'])
+async def test_prcc(message: types.Message):
+    await bot.send_message(message.chat.id, "Testing!")
+    user = str(message.chat.id)
+    print("str(messageID):",user)
+    print('type:',type(user))
+    client.set(user, "bot_test", 30)
+    await asyncio.sleep(1)
+    testget = client.get(str(message.chat.id))
+    logger.debug(testget)
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp,skip_updates=True)
